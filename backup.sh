f31970bce66d4e08a3761a9ad0f77bb6279658a9
Dietrich Rordorf <rordorf@ditoy.com>
2018-11-12 15:03:56 +0100

    change backup filenames

--- backup.sh
+++ backup.sh
@@ -7,15 +7,15 @@ echo "[($date)] starting the backup process"
 cd /backup
 
 echo "[($date)] taring the backup folder..."
-tar cJf ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz ./*
+tar cJf ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz ./*
 cd /
 
 echo "[($date)] encrypting the backup file..."
-gpg --trust-model always --output ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg --encrypt --recipient $GPG_RECIPIENT ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz
-rm ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz
+gpg --trust-model always --output ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz.gpg --encrypt --recipient $GPG_RECIPIENT ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz
+rm ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz
 
 echo "[($date)] uploading encrypted backup file to AWS S3..."
-aws s3 cp ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg s3://$S3_BUCKET_NAME/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg --storage-class STANDARD_IA
-rm ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg
+aws s3 cp ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz.gpg s3://$S3_BUCKET_NAME/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz.gpg --storage-class STANDARD_IA
+rm ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz.gpg
 
 echo "[($date)] backup process completed"
