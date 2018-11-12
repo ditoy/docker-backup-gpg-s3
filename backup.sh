fffa42e486bf6fcc1c016a7475734a639ffcfe9d
Dietrich Rordorf <rordorf@ditoy.com>
2018-11-12 14:57:34 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,12 +2,20 @@
 
 BACKUP_DATE=$(date +"%Y-%m-%d_%H-%M")
 
+echo "[($date)] starting the backup process"
+
 cd /backup
+
+echo "[($date)] taring the backup folder..."
 tar cJf ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz ./*
 cd /
 
+echo "[($date)] encrypting the backup file..."
 gpg --trust-model always --output ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg --encrypt --recipient $GPG_RECIPIENT ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz
 rm ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz
 
+echo "[($date)] uploading encrypted backup file to AWS S3..."
 aws s3 cp ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg s3://$S3_BUCKET_NAME/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg --storage-class STANDARD_IA
 rm ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg
+
+echo "[($date)] backup process completed"
