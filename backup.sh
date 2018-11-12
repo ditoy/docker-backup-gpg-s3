70fc75f200bda3842ab7ce14596e77046b9f32eb
Dietrich Rordorf <rordorf@ditoy.com>
2018-11-12 16:11:47 +0100

    use a filename var with APP_NAME in it to do the backups

--- backup.sh
+++ backup.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
 BACKUP_DATE=$(date +"%Y-%m-%d_%H-%M")
+FILENAME=$BACKUP_DATE-$APP_NAME
 
 echo "[`date +%Y-%m-%d\ %H:%M:%S`] starting the backup process"
 
 cd /backup
 
 echo "[`date +%Y-%m-%d\ %H:%M:%S`] taring the backup folder..."
-tar cJf ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz ./*
+tar cJf ~/$FILENAME.tar.xz ./*
 cd /
 
 echo "[`date +%Y-%m-%d\ %H:%M:%S`] encrypting the backup file..."
-gpg --trust-model always --output ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz.gpg --encrypt --recipient $GPG_RECIPIENT ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz
-rm ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz
+gpg --trust-model always --output ~/$FILENAME.tar.xz.gpg --encrypt --recipient $GPG_RECIPIENT ~/$FILENAME.tar.xz
+rm ~/$FILENAME.tar.xz
 
 echo "[`date +%Y-%m-%d\ %H:%M:%S`] uploading encrypted backup file to AWS S3..."
-aws s3 cp ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz.gpg s3://$S3_BUCKET_NAME/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz.gpg --storage-class STANDARD_IA
-rm ~/$BACKUP_DATE-$S3_BUCKET_NAME.tar.xz.gpg
+aws s3 cp ~/$FILENAME.tar.xz.gpg s3://$S3_BUCKET_NAME/$FILENAME.tar.xz.gpg --storage-class STANDARD_IA
+rm ~/$FILENAME.tar.xz.gpg
 
 echo "[($date)] backup process completed"
