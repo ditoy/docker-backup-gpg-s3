bc24c44ebac1bfe187a7487ab198fa4042d17302
Dietrich Rordorf <rordorf@ditoy.com>
2019-08-30 14:45:13 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 # Import GPG public keys
+echo "Importing GPG public keys..."
 gpg --import /keys/*
 
 # Create and install crontab file
+echo "Installing crontab..."
 echo "$CRON_INTERVAL /backup.sh" >> /backup.cron
 
+echo "Launching crontab..."
 crontab /backup.cron
 
 tail -f /dev/null
